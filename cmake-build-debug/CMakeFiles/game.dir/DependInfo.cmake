35e4a01d018fc2dbd6e3f68129f46fc0de5b287a
utec <americo.auccacusi@utec.edu.pe>
2019-06-21 19:07:12 -0500

    Segunda actualizacion

---
+++ cmake-build-debug/CMakeFiles/game.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/utec/POO1-Proyecto-Final/Menu.cpp" "/home/utec/POO1-Proyecto-Final/cmake-build-debug/CMakeFiles/game.dir/Menu.cpp.o"
+  "/home/utec/POO1-Proyecto-Final/Objeto.cpp" "/home/utec/POO1-Proyecto-Final/cmake-build-debug/CMakeFiles/game.dir/Objeto.cpp.o"
+  "/home/utec/POO1-Proyecto-Final/Tierra.cpp" "/home/utec/POO1-Proyecto-Final/cmake-build-debug/CMakeFiles/game.dir/Tierra.cpp.o"
+  "/home/utec/POO1-Proyecto-Final/Tipos.cpp" "/home/utec/POO1-Proyecto-Final/cmake-build-debug/CMakeFiles/game.dir/Tipos.cpp.o"
+  "/home/utec/POO1-Proyecto-Final/main.cpp" "/home/utec/POO1-Proyecto-Final/cmake-build-debug/CMakeFiles/game.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/game.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/game.dir/main.cpp.o"
+  "CMakeFiles/game.dir/Tierra.cpp.o"
+  "CMakeFiles/game.dir/Objeto.cpp.o"
+  "CMakeFiles/game.dir/Tipos.cpp.o"
+  "CMakeFiles/game.dir/Menu.cpp.o"
+  "game.pdb"
+  "game"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
